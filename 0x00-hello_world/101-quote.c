b9b6242dc4b5bc18e884771156f4b06fe0311331
Habinger <55584446+Habinger@users.noreply.github.com>
2022-03-10 19:49:55 +0100

    initial

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/**
+ * main - print and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: 1 to standard error
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - print with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
