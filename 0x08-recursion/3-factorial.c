ae98725964320e1cf36c493a94bba445f414e986
Habinger <55584446+Habinger@users.noreply.github.com>
2022-03-30 20:03:05 +0100

    initial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of a number
+ *
+ * @n: input variable
+ *
+ * Return: factorial Success
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
